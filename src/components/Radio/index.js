0cf0583db3475d4ccf297abb1c7aa028799d2789
emontes <emontes@turista.com.mx>
2021-11-02 21:27:40 -0600

    Ãactualizacion de direccion de estacion

--- src/components/Radio/index.js
+++ src/components/Radio/index.js
@@ -1,14 +1,14 @@
-import React from "react"
-import "./Radio.scss"
+import React from "react";
+import "./Radio.scss";
 const Radio = ({ videoSrcURL, videoTitle, ...props }) => (
   <div>
     <video className="radio-box" controls autoplay="">
-    <source 
-    src="http://192.99.6.50:9960/stream?type=http&amp;nocache=2" 
-    type="audio/mpeg"
-    />
+      <source
+        src="https://stream9.mexiserver.com/8216/stream"
+        type="audio/mpeg"
+      />
     </video>
   </div>
 );
 
-export default Radio
+export default Radio;
