e3a597161b47e31c5fee2dc065778adf23b46a2e
emontes <emontes@turista.com.mx>
2022-04-10 20:38:22 -0500

    cambio en  url del reproductor de radio

--- src/components/Radio/index.js
+++ src/components/Radio/index.js
@@ -1,14 +1,11 @@
-import React from "react";
-import "./Radio.scss";
+import React from 'react'
+import './Radio.scss'
 const Radio = ({ videoSrcURL, videoTitle, ...props }) => (
   <div>
     <video className="radio-box" controls autoplay="">
-      <source
-        src="https://stream9.mexiserver.com/8216/stream"
-        type="audio/mpeg"
-      />
+      <source src="https://stream9.mexiserver.com/8216/;" type="audio/mpeg" />
     </video>
   </div>
-);
+)
 
-export default Radio;
+export default Radio
